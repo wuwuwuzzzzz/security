6e6300ace50fb02a40486771b41459b8b6fda353
wxz <759126349@qq.com>
2024-02-01 21:26:34 +0800

    优化代码

--- src/main/java/com/wxz/security/controller/IndexController.java
+++ src/main/java/com/wxz/security/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.wxz.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wxz
