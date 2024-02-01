82150ed7feaf0884ab617acfdbaae5c24186fae1
wxz <759126349@qq.com>
2024-02-01 21:26:14 +0800

    init

--- src/main/java/com/wxz/security/SecurityApplication.java
+++ src/main/java/com/wxz/security/SecurityApplication.java
@@ -3,11 +3,20 @@ package com.wxz.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wxz
+ * @date 21:14 2024/2/1
+ */
 @SpringBootApplication
-public class SecurityApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
-
+public class SecurityApplication
+{
+    /**
+     * @param args args
+     * @author wxz
+     * @date 21:14 2024/2/1
+     */
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 }
--- src/main/java/com/wxz/security/controller/IndexController.java
+++ src/main/java/com/wxz/security/controller/IndexController.java
@@ -1,2 +1,24 @@
-package com.wxz.security.controller;public class IndexController {
+package com.wxz.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wxz
+ * @date 21:04 2024/2/1
+ */
+@Controller
+public class IndexController
+{
+    /**
+     * @return java.lang.String
+     * @author wxz
+     * @date 21:04 2024/2/1
+     */
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
 }
